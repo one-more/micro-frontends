750a9a18c0cc5914f19973d9173c901ea4c5a5de
one-more <greyjoi@yandex.ru>
2019-09-04 12:27:25 +0300

    build core package before publish

---
+++ packages/core/webpack.config.js
@@ -0,0 +1,9 @@
+const merge = require('webpack-merge'),
+    config = require('../../webpack.config');
+
+module.exports = merge(config, {
+    entry: __dirname + '/index.ts',
+    output: {
+        path: __dirname + "/dist",
+    },
+});
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    mode: 'production',
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "awesome-typescript-loader",
+                options: {
+                    "useBabel": true,
+                    "babelOptions": {
+                        "babelrc": true,
+                        "presets": [
+                            ["@babel/preset-env", { "targets": "last 2 versions, ie 11", "modules": false }]
+                        ]
+                    },
+                    "babelCore": "@babel/core",
+                }
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+};
